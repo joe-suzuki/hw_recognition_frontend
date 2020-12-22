967e887cbc7eed80b1336b4dbe53b1e1b320655a
teaching <teaching@tsuzukinoiMac.local>
2020-12-22 19:30:10 +0900

    created canvas and buttons

--- src/components/HandWritten.js
+++ src/components/HandWritten.js
@@ -1,10 +1,25 @@
 import React from "react"
 
+import styles from "./HandWritten.module.scss"
+
 const HandWritten = () => {
+  const canvasWidth = 28
+  const canvasHeight = 28
 
   return(
-    <div>
+    <div className={styles.wrapper}>
       <h1>Hand Written Recognition</h1>
+
+      <canvas
+        width={canvasWidth}
+        height={canvasHeight}
+        className={styles.canvas}
+      />
+
+      <div className={styles.buttons_wrapper}>
+        <button>reset</button>
+        <button>predict</button>
+      </div>
     </div>
   )
 }
