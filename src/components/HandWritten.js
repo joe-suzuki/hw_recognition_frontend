d56754e848dbb28ac831e3e00b1cff38e133826f
teaching <teaching@tsuzukinoiMac.local>
2020-12-28 03:53:57 +0900

    refactor calling api

--- src/components/HandWritten.js
+++ src/components/HandWritten.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react"
+import React, {useRef, useState, useEffect} from "react"
 import axios from "axios"
 
 import {Bar} from "react-chartjs-2"
@@ -12,6 +12,25 @@ const HandWritten = () => {
 
   const [predictedLabel, setPredictedLabel] = useState("")
   const [predictionProb, setPredictionProb] = useState([])
+
+  const [base64String, setBase64String] = useState("")
+
+  useEffect(() => {
+    if(base64String){
+      (async () => {
+        const response = await axios.post("http://localhost:8000/api/predict", {
+          "image": base64String
+        }).catch(error => {
+          console.log("an error occurred while predicting")
+        })
+
+        setPredictedLabel(response.data.predicted_label)
+        setPredictionProb(response.data.prediction_prob)
+
+        console.log(response)
+      })()
+    }
+  }, [base64String])
 
   const data = {
     labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
@@ -47,23 +66,14 @@ const HandWritten = () => {
   }
   
   const predict = async () => {
-    const base64String = canvasRef.current.toDataURL("image/png")
-    console.log(base64String)
-    
-    const response = await axios.post("http://localhost:8000/api/predict", {
-      "image": base64String
-    }).catch(error => {
-      console.log("an error occurred while predicting")
-    })
-
-    setPredictedLabel(response.data.predicted_label)
-    setPredictionProb(response.data.prediction_prob)
-
-    console.log(response)
+    const base64 = canvasRef.current.toDataURL("image/png")
+    console.log(base64)
+    setBase64String(base64)
   }
 
   const handleReset = () => {
     clearDrawing()
+    setBase64String("")
 
     setPredictedLabel("")
     setPredictionProb([])
